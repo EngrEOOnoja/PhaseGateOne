3a38fc67b3874b0cbca82b47e943e9f9b76f6410
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-08-23 19:33:23 +0100

    progress

--- Java/MensturalCycleCalculator.java
+++ Java/MensturalCycleCalculator.java
@@ -1,22 +1,41 @@
 public class MensturalCycleCalculator{
+static Scanner input = new Scanner(System.in){
 
 
 
 
-public static int getNextPredictedPeriod(){
 
 
-}
-
-public static int getFertileWindow(){
-
-
+public static int getNextPredictedPeriod(userInput1){
 
 
 }
 
 
-public static int getPredictedOvulationDate(){
+public static int getLeapYearStatus(){
+	if (number1 > 1000 || number1 > ) {
+	System.out.println("Invalid  Number is not a current Year");
+	}
+	else if  return (number1 % 4 == 0 && number1 % 100 !=0) || number1 % 400 == 0) {
+	System.out.println("True");
+	}
+	
+	else {
+	System.out.println("Flase");
+	}
+
+
+}
+
+public static int getFertileWindow(userInput2){
+
+
+
+
+}
+
+
+public static int getPredictedOvulationDate(userInput3){
 
 
 
@@ -24,3 +43,6 @@ public static int getPredictedOvulationDate(){
 
 
 }
+
+
+	
